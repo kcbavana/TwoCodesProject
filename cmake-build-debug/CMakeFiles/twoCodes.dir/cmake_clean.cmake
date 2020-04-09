0dc79eb6658a41b53fc467e9709b5ef88c165846
kcbavana <53190642+kcbavana@users.noreply.github.com>
2020-04-08 19:44:14 -0500

    Final Version of TwoCodes
    Pair Programming Partner: Madhava Sai Yamike

---
+++ cmake-build-debug/CMakeFiles/twoCodes.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/twoCodes.dir/main.cpp.obj"
+  "libtwoCodes.dll.a"
+  "twoCodes.exe"
+  "twoCodes.exe.manifest"
+  "twoCodes.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/twoCodes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
